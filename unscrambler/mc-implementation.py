f8689ca132a64f26d99e2f8948458d2e85e29824
George <38766881+George-Leonard@users.noreply.github.com>
2020-10-09 22:56:37 +0100

    Add files via upload

---
+++ unscrambler/mc-implementation.py
@@ -0,0 +1,106 @@
+import os, time, pyperclip, unscrambler as u
+log = open(os.getenv("APPDATA") +"/.minecraft/logs/latest.log", "r")
+
+arkhamwords = []
+u.addword("Wolverine", arkhamwords)
+u.addword("playervaults", arkhamwords)
+u.addword("Arkham Network", arkhamwords)
+u.addword("Youtube", arkhamwords)
+u.addword("Legend", arkhamwords)
+u.addword("towny", arkhamwords)
+u.addword("steak", arkhamwords)
+u.addword("pandas", arkhamwords)
+u.addword("superman", arkhamwords)
+u.addword("hulk", arkhamwords)
+u.addword("auction", arkhamwords)
+u.addword("Twitter", arkhamwords)
+u.addword("villager", arkhamwords)
+u.addword("lightning", arkhamwords)
+u.addword("message", arkhamwords)
+u.addword("oldschool-prison", arkhamwords)
+u.addword("scrambler", arkhamwords)
+u.addword("events", arkhamwords)
+u.addword("Discord", arkhamwords)
+u.addword("black-panther", arkhamwords)
+u.addword("harvest", arkhamwords)
+u.addword("Play-mc", arkhamwords)
+u.addword("creepers", arkhamwords)
+    
+
+while True:
+    line = log.readline()
+
+
+#------------------------------------------------------------------------------
+#                               Reaction word
+#------------------------------------------------------------------------------
+    if "[Client thread/INFO]: [CHAT] Reaction Word:" in line:
+        rlist = list(line)
+        print(line)
+        rword = rlist[55:]
+        rword = rword[:(len(rword)-1)]
+        for c in rword:
+            for i in (" [],''"):
+                rword = str(rword).replace(i, '')
+
+
+        if not line:
+            time.sleep(0.1)
+            continue
+
+
+
+
+        print(rword)
+        pyperclip.copy(rword) 
+        
+#------------------------------------------------------------------------------
+#                              Math Problem
+#------------------------------------------------------------------------------
+
+    if "[Client thread/INFO]: [CHAT] Math problem:" in line:
+
+        mlist = list(line)
+        print(line)
+        mquestion = mlist[54:]
+        mquestion = mquestion[:(len(mquestion)-1)]
+        for c in mquestion:
+            for i in (" [],''"):
+                mquestion = str(mquestion).replace(i,'')
+
+        if not line:
+            time.sleep(0.1)
+            continue
+
+
+
+        print(eval(mquestion))
+        pyperclip.copy(eval(mquestion))
+        
+#------------------------------------------------------------------------------
+#                             Unscramble word
+#------------------------------------------------------------------------------
+# WORDS : Wolverine, playervaults, Arkham Network, YouTube, Legend, towny, steak, pandas, superman, hulk, auction, Twitter, villager, lightning, message, 
+#         oldschool-prison, scrambler, events, Discord, black-panther, harvest, Play-mc, creepers
+
+    if "[Client thread/INFO]: [CHAT] Scrambled word:" in line:
+        swlist = list(line)
+        print(line)
+        sw = swlist[56:]
+        sw = sw[:(len(sw)-1)]
+        #print(sw)
+        for c in sw:
+            for i in (" [],''"):
+                sw = str(sw).replace(i,'')
+
+
+        text = u.unscramble(sw, arkhamwords))
+        print(text)
+        pyperclip.copy(text)
+
+    if not line:
+        time.sleep(0.1)
+        continue
+
+                            
+
---
+++ unscrambler/unscrambler main.py
@@ -0,0 +1,46 @@
+def addword(word, array):
+    array.append(list(word))
+
+def points(wordindex, array, compareword):
+    word = array[int(wordindex)]
+    pointnumber = 0
+    if len(array[wordindex]) == len(compareword):
+        pointnumber = pointnumber+1
+        for c in word:
+            if c in compareword:
+                pointnumber = pointnumber+1
+        return pointnumber
+    else:
+        for c in word:
+            if c in compareword:
+                pointnumber = pointnumber+1
+        return pointnumber        
+
+def unscramble(word, array):
+    pointlist = []
+    print(word)
+  
+    for i in range(len(array)):
+        t = points(i, array, word)
+        pointlist.append(t)
+    for i in pointlist:
+        
+        if i == len(word)+1 and len(array[pointlist.index(i)]) == len(word):
+            print("Exact match is:", array[pointlist.index(i)])
+            unscrambled = array[pointlist.index(i)]
+            for c in unscrambled:
+                for i in (" [],''"):
+                    unscrambled = str(unscrambled).replace(i, '')
+            return str(unscrambled)
+            break
+        if max(pointlist) == 0 or max(pointlist) == 1:
+            print("No similar words found. Add to your list!")
+            print("")
+        else:
+            print(str(array[pointlist.index(i)]) + "is a close match, it has: " + str(i) + "points")
+
+test = []
+addword("oldschool-prison", test)
+addword("thor", test)
+test = unscramble("tohr", test)
+print(test)
