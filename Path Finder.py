4248b9ae45dfbec05fb2e900971e004123df81e4
George <38766881+George-Leonard@users.noreply.github.com>
2021-02-27 07:32:08 +0000

    Update Path Finder.py

--- Path Finder.py
+++ Path Finder.py
@@ -2,16 +2,13 @@ import numpy as np
 import random
 import time
 
+#size = input('Dimensions(x, y): ')
+size = '10,10'
+#start = input('Start point(x,y): ')
+start = '0,0'
+#end = input('End point(x,y): ')
+end = '9,9'
 
-#user friendly
-
-size = input('Dimensions(x, y): ')
-start = input('Start point(x,y): ')
-end = input('End point(x,y): ')
-
-#size = '10,10'
-#start = '9,0'
-#end = '0,9'
 
 
 
@@ -37,8 +34,6 @@ print(maze)
 x_ = 1
 obstacleslist = []
 
-#user friendly
-
 while x_==1:
     obstacles = input('Obstacles(x,y): ')
     if obstacles == '-':
@@ -58,54 +53,82 @@ for i in range(0,len(obstacleslist)):
         #--------------------------------------------
         maze[int(x)][int(y)] = 2
         #--------------------------------------------
-    
+
 finished = False
 
-already = []
-avail = []
-while finished == False:
-
-    newcells = []
-    for i in range(0, len(maze)):
-        for x in range(0, len(maze[i])):
-            #print([i],[x])
-            #print(maze[x][i])
-            avail.append([x,i])
-
-    for i in range(0,len(avail)):
-        w = avail[i][0]
-        z = avail[i][1]
-        if w-1==start[0] or z-1==start[1] or w+1==start[0] or z+1==start[1]:
-            if z==start[1]:
-                newcells.append(avail[i])
-            if w==start[0]:
-                newcells.append(avail[i])
-            else:
-                pass
-
-    try:
-        start = random.choice(newcells)
-        if maze[start[1]][start[0]] != 2:
-            maze[start[1]][start[0]] = 1
-            print(maze)
-        if start[1] == end[1] and end[0]-start[0]==1:
-            print('finished')
-            break
-        if start[0] == end[0] and end[1]-start[1]==1:
-            print('finished')
-            break
-    except:
-        break
 print(maze)
-        
-        
-        
+
+
+#-------------------------------------
+
+#heuristics
+hs = [] # approx distances from end
+
+nodes = []
+nodes.append([start[1],start[0]]) #begin at the start point
+
+
+#i want a possibilities in every direction one node away from the node. i.e:
+#x x x
+#x N x
+#x x x
+g = True
+
+while g == True:
+    possibilities = [] #all possibilities for a specific node
+    hs = []
+    for i in range(0, len(nodes)):
+        x = nodes[i][0]
+        y = nodes[i][1]
+
+        if x-1 >= start[1] and y+1 >= start[1] and x-1 <= end[1] and y+1 <= end[1]:
+            possibilities.append([x-1,y+1])
+        if x >= start[1] and y+1 >= start[1] and x <= end[1] and y+1 <= end[1]:
+            possibilities.append([x,y+1])
+        if x+1 >= start[1] and y+1 >= start[1] and x+1 <= end[1] and y+1 <= end[1]:
+            possibilities.append([x+1,y+1])
+        if x-1 >= start[1] and y >= start[1] and x-1 <= end[1] and y <= end[1]:
+            possibilities.append([x-1,y])
+        if x+1 >= start[1] and y >= start[1] and x+1 <= end[1] and y <= end[1]:
+            possibilities.append([x+1,y])
+        if x-1 >= start[1] and y-1 >= start[1] and x-1 <= end[1] and y-1 <= end[1]:
+            possibilities.append([x-1,y-1])
+        if x >= start[1] and y-1 >= start[1] and x <= end[1] and y-1 <= end[1]:
+            possibilities.append([x,y-1])
+        if x+1 >= start[1] and y-1 >= start[1] and x+1 <= end[1] and y-1 <= end[1]:
+            possibilities.append([x+1,y-1])
+
+
+    for i in possibilities:
+        if i not in obstacleslist:
+            if i not in nodes:
+
+                xs = end[0]-i[0]
+                ys = end[1]-i[1]
+
+                hs.append(np.sqrt(xs+ys))
                 
-    
 
-
-print(maze)
         
+    for i in possibilities:
+        if i not in obstacleslist:
+            if i not in nodes:
+                xs = end[0]-i[0]
+                ys = end[1]-i[1]
+                if np.sqrt(xs+ys) == min(hs):
+                    nodes.append(i)
+                    maze[i[1]][[i[0]]] = '9'
+
+                
+        if str(i) == str(end):
+            g = False
+            maze[i[1]][[i[0]]] = '1'
+
+    print(maze)
+    print(nodes)
+
+
+
         
         
                 
